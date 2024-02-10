f44270bb4aed176eb5def46952bc05e7255d7a7d
Nafisa <Nafisafaruna@gmail.com>
2024-02-10 12:16:27 +0100

    setup dev environment

---
+++ digital-course/src/App.tsx
@@ -0,0 +1,11 @@
+import NewsLetter from "./Newsletter.tsx";
+
+function App () {
+  return (
+    <>
+     <NewsLetter/>
+    </>
+  )
+}
+
+export default App;
---
+++ digital-course/src/Newsletter.tsx
@@ -0,0 +1,9 @@
+function NewsLetter () {
+  return (
+    <>
+     <h2 className="text-center ">Enroll in the 7-week online biz mastermind</h2>
+    </>
+  )
+}
+
+export default NewsLetter;
